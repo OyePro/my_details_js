1d2e51af4e90662b3fa41086d4c9ed78ee55112e
Olalekan Okewale <olalekantoheeb199@gmail.com>
2022-08-01 04:22:47 +0100

    my details

--- node.js
+++ node.js
@@ -1,13 +1,28 @@
-//my names are;
 const firstName = 'Olalekan'
 const middleName = 'Toheeb'
 const lastName = 'Okewale'
-//getting my fullname
 const fullName = `${firstName} ${middleName} ${lastName}`
-console.log("My name is " + fullName);
-//my height;
+
+document.getElementById("name").innerHTML = fullName;
+
 const myHeight = 1.75 + ' meter'
-console.log("I'm " + myHeight + " tall.");
-//the name of my country
+
+document.getElementById("height").innerHTML = myHeight;
+
 const myCountryName = 'Nigeria'
-console.log("The name of my country is " + myCountryName);
+
+document.getElementById("country").innerHTML = myCountryName;
+
+// //my names are;
+// const firstName = 'Olalekan'
+// const middleName = 'Toheeb'
+// const lastName = 'Okewale'
+// //getting my fullname
+// const fullName = `${firstName} ${middleName} ${lastName}`
+// console.log("My name is " + fullName);
+// //my height;
+// const myHeight = 1.75 + ' meter'
+// console.log("I'm " + myHeight + " tall.");
+// //the name of my country
+// const myCountryName = 'Nigeria'
+// console.log("The name of my country is " + myCountryName);
